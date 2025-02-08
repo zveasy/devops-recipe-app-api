d7b65c88466e830d1555ec9aad002eddfc71601f
Zak Demo <zveasy327@gmail.com>
2025-02-08 00:01:38 -0600

    more changes

--- infra/setup/output.tf
+++ infra/setup/output.tf
@@ -8,3 +8,14 @@ output "cd_user_access_key_secret" {
   value       = aws_iam_access_key.cd.secret
   sensitive   = true
 }
+
+
+output "ecr_repo_app" {
+  description = "ECR repository URL for app image"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+output "ecr_repo_proxyy" {
+  description = "ECR repository URL for the proxyy image"
+  value       = aws_ecr_repository.proxyy.repository_url
+}
