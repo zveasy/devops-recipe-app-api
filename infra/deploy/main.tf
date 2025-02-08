7badcee9404de0d93355ee93844d04461ef65f90
Zak Demo <zveasy327@gmail.com>
2025-02-07 23:03:45 -0600

    Setup Terraform

---
+++ infra/deploy/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket               = "devops-app-zcv"
+    key                  = "tf-state-setup"
+    workspace_key_prefix = "tf-state-deploy-env"
+    region               = "us-east-1"
+    encrypt              = true
+    dynamodb_table       = "devops-zcv-lock"
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/deploy"
+    }
+  }
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+}
+
+
+data "aws_region" "current" {}
---
+++ infra/setup/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "devops-app-zcv"
+    key            = "tf-state-setup"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "devops-zcv-lock"
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
+}
