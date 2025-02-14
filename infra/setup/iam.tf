ef37c80390eac1f204b3799fccf86f2b46483100
Zak Demo <zveasy327@gmail.com>
2025-02-14 11:08:38 -0600

    updated iam user

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -24,10 +24,7 @@ data "aws_iam_policy_document" "tf_backend" {
   statement {
     effect  = "Allow"
     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
-    resources = [
-      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
-      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
-    ]
+    resources = ["arn:aws:s3:::${var.tf_state_bucket}/*"]
   }
   statement {
     effect = "Allow"
